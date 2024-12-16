4b0666c9c06434d131b73fef4213e0601f29617e
rogi39 <igor93osetia@yandex.ru>
2024-12-16 13:15:02 +0300

    search

--- wp-content/themes/pixbrowse/front-page.php
+++ wp-content/themes/pixbrowse/front-page.php
@@ -4,14 +4,7 @@
 		<div class="container">
 			<h1 class="hero__title title title_tac">Discover a World of Visual Inspiration</h1>
 			<div class="hero__text">Discover and download top-quality photos, designs, and mockups.</div>
-			<form class="search">
-				<input type="text" class="search__input" placeholder="Search images">
-				<button class="search__btn">
-					<svg class="search__btn-svg" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SearchIcon">
-						<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-					</svg>
-				</button>
-			</form>
+			<?php echo get_template_part("template_parts/search"); ?>
 		</div>
 	</div>
 	<img src="<?php echo get_template_directory_uri(); ?>/images/dest/picture.jpg" alt="pic" class="main-pic">
--- wp-content/themes/pixbrowse/single-photos.php
+++ wp-content/themes/pixbrowse/single-photos.php
@@ -2,14 +2,7 @@
 <main>
 	<div class="section section_photo">
 		<div class="container">
-			<form class="search search_w100 search_mb">
-				<input type="text" class="search__input" placeholder="Search images">
-				<button class="search__btn">
-					<svg class="search__btn-svg" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="SearchIcon">
-						<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-					</svg>
-				</button>
-			</form>
+			<?php echo get_template_part("template_parts/search", null, ['width' => 'w100']); ?>
 			<h1 class="title title_fw400"><?php the_title(); ?></h1>
 
 			<?php
