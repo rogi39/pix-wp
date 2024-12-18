32be04809af7afa27d32f4978ac83af1cb18aa7e
rogi39 <igor93osetia@yandex.ru>
2024-12-18 16:39:49 +0300

    page

--- wp-content/themes/pixbrowse/page.php
+++ wp-content/themes/pixbrowse/page.php
@@ -1,3 +1,8 @@
-<?php get_header();?>
-<?php /*echo get_template_part("template_parts/block");*/ ?>
+<?php get_header(); ?>
+<div class="section section_photo">
+	<div class="container">
+		<h1 class="title title_fw400"><?php the_title(); ?></h1>
+		<div class="content"><?php the_content(); ?></div>
+	</div>
+</div>
 <?php get_footer(); ?>
