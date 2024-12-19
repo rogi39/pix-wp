ca55a2f0b0d431719a5199951d382101508794ae
rogi39 <igor93osetia@yandex.ru>
2024-12-19 15:13:54 +0300

    about

---
+++ wp-content/themes/pixbrowse/page-about-us.php
@@ -0,0 +1,91 @@
+<?php get_header(); ?>
+<main>
+	<div class="section section_pt0">
+		<div class="container">
+			<div class="about-hero">
+				<div class="about-hero__title">PixBrowse</div>
+				<div class="about-hero__text">Your massive library of high-quality images for you to explore.</div>
+				<div class="about-hero__btn-row">
+					<a href="#" class="about-hero__btn">Sign up</a>
+					<a href="#" class="about-hero__btn about-hero__btn_reverse">Start browsing</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section section_pt0">
+		<div class="container">
+			<div class="achieve">
+				<div class="achieve__title">Achieve your goals <span>with our visual library:</span></div>
+				<div class="achieve__item">
+					<div class="achieve__item-title">Our start</div>
+					<div class="achieve__item-text">PixBrowse was born out of a passion for visual storytelling and a desire to make it easier for everyone to find the images they need to tell their stories. Founded in 2024 by a team of visual experts and content creators in Tallinn, Estonia, PixBrowse is a platform that provides access to a wide range of high-quality images, as well as tools and resources to help people create their own.</div>
+				</div>
+				<div class="achieve__item">
+					<div class="achieve__item-title">What drives us</div>
+					<div class="achieve__item-text">We are on a journey to create and preserve high-quality visual content, empowering people around the world to communicate visually, with a focus on the European market.</div>
+				</div>
+				<div class="achieve__item">
+					<div class="achieve__item-title">Growing the hub</div>
+					<div class="achieve__item-text">As a new hub for visual storytellers, we are empowering the creative economy by identifying and responding to the latest cultural shifts, global trends, and market dynamics. We welcome both professionals and hobby content creators to our platform to connect and share their work.</div>
+				</div>
+				<div class="achieve__item">
+					<div class="achieve__item-title">No one left behind</div>
+					<div class="achieve__item-text">We are a passionate team of storytellers who help our clients create visual content that engages and inspires their audiences. We believe that the power of storytelling is essential for building strong brands and lasting relationships.</div>
+				</div>
+				<div class="achieve__try">
+					<div class="achieve__try-title">Try our PixBrowse</div>
+					<a href="#" class="achieve__try-btn">Sign Up</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section section_pt0">
+		<div class="container">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/dest/about-team.png" alt="about">
+			<div class="about-craft">
+				<div class="about-craft__title">Craft something extraordinary</div>
+				<div class="about-craft__text">Motivated by our own struggle to find high-quality, useful images, we created PixBrowse to fill a need for artists around the world. Today, thousands of artists have downloaded PixBrowse photos to use in their presentations, artwork, mockups, and other projects.</div>
+				<div class="about-craft__btn-block">
+					<a href="#" class="about-craft__btn">Sign Up</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+
+	<div class="section section_pb0 section_stock">
+		<div class="container">
+			<div class="about-stock">
+				<div class="about-stock__title">PixBrowse is the perfect place to explore your creativity, with a wide range of content to inspire you.</div>
+				<div class="about-stock__row">
+					<div class="about-stock__left">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/dest/about-stock.png" alt="about">
+					</div>
+					<div class="about-stock__right">
+						<div class="about-stock__item">
+							<div class="about-stock__item-title">A wide variety of royalty-free photos</div>
+							<div class="about-stock__item-text">Our library of curated photos is so vast and diverse, you're sure to find what you need, no matter what.</div>
+						</div>
+						<div class="about-stock__item">
+							<div class="about-stock__item-title">Users from all corners of the globe</div>
+							<div class="about-stock__item-text">PixBrowse is a bustling space where photographers from all around the globe, encompassing amateurs, professionals, budding enterprises, and a diverse range of enthusiasts, come together to connect and display their photographic talents.</div>
+						</div>
+						<div class="about-stock__item">
+							<div class="about-stock__item-title">Effective customer service</div>
+							<div class="about-stock__item-text">Whenever you have inquiries, rest assured that our PixBrowse customer support team is available day and night, ready to assist you.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<div class="footer-block">Hold on to the high-quality images you have a fondness for.</div>
+</main>
+<?php get_footer(); ?>
